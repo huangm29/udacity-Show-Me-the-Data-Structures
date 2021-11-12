c81ff834b814e6a0058c858c3353924e10e5007f
Meng Huang <huangm1065@gmail.com>
2021-11-11 23:31:33 -0500

    adding projects 456

---
+++ project_4.py
@@ -0,0 +1,52 @@
+class Group(object):
+    def __init__(self, _name):
+        self.name = _name
+        self.groups = []
+        self.users = []
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_groups(self):
+        return self.groups
+
+    def get_users(self):
+        return self.users
+
+    def get_name(self):
+        return self.name
+
+
+parent = Group("parent")
+child = Group("child")
+sub_child = Group("subchild")
+
+sub_child_user = "sub_child_user"
+sub_child.add_user(sub_child_user)
+
+child.add_group(sub_child)
+parent.add_group(child)
+
+def is_user_in_group(user, group):
+    """
+    Return True if user is in the group, False otherwise.
+
+    Args:
+      user(str): user name/id
+      group(class:Group): group to check user membership against
+    """
+    group_users = group.get_users()
+    subgroup_list = group.get_groups()
+    print(group_users)
+    if user in group_users:
+        return True
+    elif len(subgroup_list)!=0:
+        for subgroup in subgroup_list:
+            if is_user_in_group(user,subgroup):
+                return True 
+    return False
+
+print(is_user_in_group(sub_child_user, parent))
